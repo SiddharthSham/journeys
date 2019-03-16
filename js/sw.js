28c5fb5467b8da5358810fb5d3844a4125b5b2c9
Siddharth Sham <siddhsham@gmail.com>
2019-03-16 15:12:16 +0530

    service worker and manifest update

---
+++ js/sw.js
@@ -0,0 +1,86 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.0.0/workbox-sw.js');
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
+
+workbox.routing.registerRoute(
+  //cache manifest file
+  /\.(?:json)$/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'manifest-cache',
+  }),
+);
+
+workbox.routing.registerRoute(
+  //cache index file
+  /\.(?:html)$/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'index-cache',
+  }),
+);
+
+workbox.routing.registerRoute(
+  /\.(?:js|css)$/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'static-resources',
+  }),
+);
+
+workbox.routing.registerRoute(
+  // Cache image files
+  /.*\.(?:png|jpg|jpeg|svg|gif)/,
+  // Use the cache if it's available
+  workbox.strategies.cacheFirst({
+    // Use a custom cache name
+    cacheName: 'image-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        // Cache only 10 images
+        maxEntries: 10,
+        // Cache for a maximum of three days
+        maxAgeSeconds: 3 * 24 * 60 * 60,
+      })
+    ],
+  })
+);
+
+workbox.routing.registerRoute(
+  new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'google-fonts',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 4,
+      }),
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200]
+      }),
+    ],
+  }),
+);
+
+workbox.routing.setCatchHandler(({url, event, params}) => {
+  console.log("Unknown error, due to dev being lazy")
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log('Fetch event for ', event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        console.log('Found ', event.request.url, ' in cache');
+        return response;
+      }
+      console.log('Network request for ', event.request.url);
+      return fetch(event.request)
+
+    }).catch(function(error) {
+
+      return caches.match('index.html');
+
+    })
+  );
+});
